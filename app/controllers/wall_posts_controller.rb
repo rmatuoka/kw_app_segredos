2cf70cfb99860158213d5de8cea3a1493380373d
Rafael Matuoka <rmatuoka@korewa.com.br>
2012-01-23 16:17:53 -0200

    ajuste wall

--- app/controllers/wall_posts_controller.rb
+++ app/controllers/wall_posts_controller.rb
@@ -3,6 +3,6 @@ class WallPostsController < ApplicationController
   
   def publish
      graph = Koala::Facebook::API.new(@facebook_cookies["access_token"])
-     graph.put_wall_post("Os Segredos de Adriane Galisteu", {:name => params[:title], :link => "http://apps.facebook.com/segredos_de_adriane"})
+     graph.put_wall_post(params[:hint], {:name => "Os Segredos de Adriane Galisteu", :link => "http://apps.facebook.com/segredos_de_adriane"})
   end
 end
